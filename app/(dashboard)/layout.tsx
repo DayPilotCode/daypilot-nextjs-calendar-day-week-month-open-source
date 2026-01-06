31e6bded8fe033b8076fd03cd3bdb1660294ac00
chris_niklas <christian.niklas@posteo.de>
2026-01-06 16:46:39 +0100

    very pretty. working. blows my mind.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 text-gray-900">
       <Header />
       <Sidebar />
       <main className="lg:pl-64 pt-16 min-h-screen">
